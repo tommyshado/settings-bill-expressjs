1f572c35a9145544754d2b95c37944fceb789f92
Mthunzi Tom <mthunzitom.10@gmail.com>
2023-07-23 12:11:14 +0200

    import settingsBill module, body-parser external module, used bodyParser to send the submitted form into the server

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 import express from "express";
 import { engine } from "express-handlebars";
+import bodyParser from "body-parser";
+import settingsBill from "settings-bill.js";
 
 // instance of express module
 const app = express();
+// settings bill factory function instance
+const billWithSettings = settingsBill();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
 
 // to be able to configure express-handlebars use handlebars engine
 app.engine('handlebars', engine());
@@ -11,9 +20,32 @@ app.set('view engine', 'handlebars');
 // use static middleware to make the public folder public to the server
 app.use(express.static("public"));
 
+
+// ROUTES:
+
 app.get("/", (req, res) => {
     res.render("index");
 });
+
+
+app.post("/settings", (req, res) => {
+
+
+    // setting the values into the factory function
+    billWithSettings.setSettings({
+        // getting name of callCost, smsCost, warningLevel and criticalLevel
+
+        callCost: req.body.callCost,
+        smsCost: req.body.smsCost,
+        warningLevel: req.body.warningLevel,
+        criticalLevel: req.body.criticalLevel
+    })
+
+    console.log(billWithSettings.getSettings());
+
+    res.redirect("/");
+});
+
 
 const PORT = process.env.PORT || 3007;
 
