277123457343aca5069bc3004004eb3223ca68b2
Mthunzi Tom <mthunzitom.10@gmail.com>
2023-07-26 12:09:38 +0200

    in line 93 I set and invoked the timeStamp() function

--- index.js
+++ index.js
@@ -90,7 +90,7 @@ app.get("/actions", (req, res) => {
 
 app.get("/actions/:actionType", (req, res) => {
     const actionType = req.params.actionType;
-    res.render("actions", {actions: billWithSettings.actionsFor(actionType)});
+    res.render("actions", {actions: timeStamp()});
 });
 
 const PORT = process.env.PORT || 3007;
